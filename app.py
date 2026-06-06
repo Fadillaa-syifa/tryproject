e9f5b0fbc05bbc3f56e63db6f93f5b084ce90f90
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-06-06 15:08:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,17 @@ def get_base64(file):
     with open(file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
-    bg = get_base64("background.jpg")
+    bg = get_base64("images/background.jpg")
     
 page_bg = f"""
 <style>
-[data-testid="stAppViewContainer"] 
-{{  background-image: url("data:image/jpg;base64,{bg}");
+[data-testid="stAppViewContainer"] {{  
+    background-image: url("data:image/jpg;base64,{bg}");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
 }}
 
-[data-testid="stHeader"] {{
-    background: rgba(0,0,0,0);
-}}
-
-[data-testid="stSidebar"] {{
-    background: rgba(255,255,255,0.8);
 }}
 </style>
 """
