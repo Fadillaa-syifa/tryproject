19462328e2a99d6a14cfd974f27f599ad0112b6d
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-06-06 21:42:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ elif menu == "About Us":
     st.write("""
     Kami adalah tim pengembang ChemBuddy, sebuah platform edukasi yang dibuat untuk membantu mahasiswa dalam memahami dan menyelesaikan berbagai perhitungan dasar pada mata kuliah Kimia Dasar, Titimetri dan Fisika Dasar.ChemBuddy hadir sebagai solusi praktis bagi mahasiswa, khususnya tingkat pertama, yang sering menghadapi kesulitan dalam melakukan konversi dan perhitungan kimia. Dengan menyediakan fitur konversi suhu, Berat Molekul (BM), Berat Ekivalen (BE), Molaritas, Normalitas, dan PPM, kami berharap dapat membantu proses belajar menjadi lebih efektif dan efisien.Website ini dikembangkan sebagai bentuk kontribusi kami dalam memanfaatkan teknologi untuk mendukung pembelajaran sains yang lebih mudah diakses dan dipahami """)
 
-    st.caption("Tim Pengembang ChemBuddy")
+    st.write("Tim Pengembang ChemBuddy")
     st.write("""
     1. *Asyifa Fadilla* (2460335)
     2. *Muhamad Daffa Alfath* (2460425)
